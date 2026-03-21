1cf150454f77009b94bc4e235a48f20152a06dd7
fermin <kidykizyt@gmail.com>
2026-03-21 20:53:55 -0300

    Ejercicio extra terminado y probado

---
+++ Actividad1/EjExtra.py
@@ -0,0 +1,57 @@
+class Team:
+    def __init__(self, team_name):
+        self.team_name = team_name
+    
+    def get_name(self):
+        return self.team_name
+
+class Match:
+    def __init__(self, home_team, away_team, home_goals, away_goals):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.home_goals = home_goals
+        self.away_goals = away_goals
+    
+    def final_score(self):
+        if (self.home_goals > self.away_goals):
+            print(f"El equipo local {self.home_team.get_name()} gano al visitante {self.away_team.get_name()} con un resultado de {self.home_goals} - {self.away_goals} ")
+        elif(self.home_goals < self.away_goals):
+            print(f"El equipo local {self.home_team} perdio frente al visitante {self.away_team} con un resultado de {self.home_goals} - {self.away_goals} ")
+        else:
+            print(f"El equipo local {self.home_team} empato frente al visitante {self.away_team} con un resultado de {self.home_goals} - {self.away_goals} ")
+
+class Tournament:
+    teams = []
+    matchs = []
+
+    def add_team(self, team_to_add):
+        self.teams.append(team_to_add)
+
+    def search_team(self, team_name):
+        for team in self.teams:
+            if(team.get_name() == team_name):
+                return True
+                break
+        return False
+    
+    def add_match(self, home_team, away_team, home_goals, away_goals):
+        home_name = home_team.get_name()
+        away_name = away_team.get_name()
+        if(self.search_team(home_name) and self.search_team(away_name)):
+            actual_match = Match(home_team, away_team, home_goals, away_goals)
+            self.matchs.append(actual_match)
+            actual_match.final_score()
+        else:
+            print("Uno de los dos equipos no existe en esta liga")
+
+torneo = Tournament()
+
+first_team_name = (input("Ingrese el nombre del primer equipo: "))
+first_team = Team(first_team_name)
+second_team_name = (input("Ingrese el nombre del segundo equipo: "))
+second_team = Team(second_team_name)
+third_team = Team("Chacarita")
+torneo.add_team(first_team)
+torneo.add_team(second_team)
+torneo.add_match(first_team, second_team, 4, 2)
+torneo.add_match(third_team, second_team, 3, 1)
